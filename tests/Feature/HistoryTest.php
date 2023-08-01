1cbb1ae94cc6533c1c1184a0334ea51247cb8344
pav-sr <pav-sr>
2023-08-01 19:07:08 +0400

    Functional tests have been added

---
+++ tests/Feature/HistoryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Enums\ModuleStatus;
+use App\Models\Module;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class HistoryTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testCreated(): void
+    {
+        $response = $this->post('/modules', ['name' => 'module_1', 'type' => 'TEMP']);
+        $response->assertStatus(200);
+
+        $modules = Module::all();
+
+        $modules[0]->value = 10;
+        $modules[0]->status = ModuleStatus::Stopped;
+
+        $modules[0]->saveOrFail();
+
+        $data = $this->getJson('/api/history/'.$modules[0]->id)->json();
+
+        $this->assertCount(1, $data);
+        $this->assertEquals($modules[0]->value, $data[0]['value']);
+        $this->assertEquals($modules[0]->status->value, $data[0]['status']);
+    }
+}
---
+++ tests/Feature/ModuleTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Enums\ModuleType;
+use App\Models\Module;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ModuleTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testCreate(): void
+    {
+        $response = $this->post('/modules', ['name' => 'module_1', 'type' => 'TEMP']);
+        $response->assertStatus(200);
+
+        $modules = Module::all();
+
+        $this->assertEquals(1, $modules->count());
+
+        $this->assertEquals('module_1', $modules[0]->name);
+        $this->assertEquals(ModuleType::Temperature, $modules[0]->type);
+    }
+
+    public function testList(): void
+    {
+        $this->post('/modules', ['name' => 'module_1', 'type' => 'TEMP']);
+        $this->post('/modules', ['name' => 'module_2', 'type' => 'SPEED']);
+        $this->post('/modules', ['name' => 'module_3', 'type' => 'PRESSURE']);
+
+        $data = $this->getJson('/api/modules')->assertStatus(200)->json();
+
+        $this->assertCount(3, $data);
+
+        $this->assertEquals('module_1', $data[0]['name']);
+        $this->assertEquals(ModuleType::Temperature->value, $data[0]['type']);
+
+        $this->assertEquals('module_2', $data[1]['name']);
+        $this->assertEquals(ModuleType::Speed->value, $data[1]['type']);
+
+        $this->assertEquals('module_3', $data[2]['name']);
+        $this->assertEquals(ModuleType::Pressure->value, $data[2]['type']);
+    }
+}
